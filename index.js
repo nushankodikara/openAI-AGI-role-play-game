625b205f1529acba66c1546747ecf4ef0349cf2b
nushankodikara <nushankodi@gmail.com>
2023-04-22 07:52:22 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const { Configuration, OpenAIApi } = require("openai");
+
+require("dotenv").config();
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+(async () => {
+    const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [
+            { role: "user", content: "What is the largest tree in the world?" },
+        ],
+    });
+    console.log(completion.data.choices[0].message);
+})();

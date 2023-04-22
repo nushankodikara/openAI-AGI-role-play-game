d73a3d4fceb2a789c789ba3eb9b6c618cc779a61
nushankodikara <nushankodi@gmail.com>
2023-04-22 11:49:31 +0530

    Added readme express server and game functions

---
+++ README.md
@@ -0,0 +1,61 @@
+# OpenAI AGI Role Play Game
+
+This is a role-playing game that uses OpenAI's GPT-3.5-Turbo and DALL-E models to create a unique gaming experience. The game runs as a server, and the player can interact with the game by using postman or any other API client. Alternatively I'm developing a GUI for the game.
+
+## Installation
+
+To install the game, follow these steps:
+
+1. Clone the git repository to your local machine. You can do this by opening a terminal and running the following command:
+
+```bash
+git clone https://github.com/nushankodikara/openAI-AGI-role-play-game
+```
+
+2. Rename the .env.example file to .env. You can do this by running the following command:
+
+```bash
+mv .env.example .env
+```
+
+3. Add your OpenAI API key to the .env file. Open the .env file in a text editor and replace YOUR_API_KEY_HERE with your actual API key.
+
+```bash
+OPENAI_API_KEY=YOUR_API_KEY_HERE
+```
+
+4. Install the necessary dependencies. Navigate to the root directory of the project in your terminal and run the following command:
+
+```bash
+npm install
+```
+
+This will install all of the dependencies listed in the package.json file.
+
+After completing these steps, you should be ready to start the game by running the npm start command.
+
+## To get an API key for OpenAI, follow these steps:
+
+-   Go to the OpenAI [website](https://platform.openai.com/account/api-keys).
+-   Click on the "Sign up" button in the top right corner.
+-   Fill out the registration form and verify your email address.
+-   Once you have verified your email, log in to your account.
+-   Click on your username in the top right corner and select "Settings".
+-   Under the "API Keys" section, click on "Generate New Key".
+-   Copy the API key and paste it into the .env file in the cloned repository.
+
+## Usage
+
+To start the game, run the following command:
+
+```bash
+npm start
+```
+
+Contributing
+
+If you would like to contribute to the project, feel free to submit a pull request with your changes. Before doing so, please read the CONTRIBUTING.md file for guidelines on how to contribute.
+
+License
+
+This project is licensed under the MIT License. See the LICENSE file for more information.
--- index.js
+++ index.js
@@ -1,18 +1,99 @@
+const express = require("express");
 const { Configuration, OpenAIApi } = require("openai");
-
 require("dotenv").config();
+
+const app = express();
+const port = 3000;
 
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
+
 const openai = new OpenAIApi(configuration);
 
-(async () => {
-    const completion = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo",
-        messages: [
-            { role: "user", content: "What is the largest tree in the world?" },
-        ],
-    });
-    console.log(completion.data.choices[0].message);
-})();
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+    try {
+        const message = "start a random adventure story";
+        const completion = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [
+                {
+                    role: "system",
+                    content:
+                        'You are an adventure game AI. please return the data in the following format only {"image":"description for a story related image", "story":"continue the story...", "enemy":trueOrFalse, "playerHealth":"x/100", "enemyHealth":"x/100", "options":["option1","option2","option3","option4"] }',
+                },
+                { role: "user", content: message },
+            ],
+        });
+        res.send(completion.data.choices[0].message.content);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error);
+    }
+});
+
+function extractJsonFromString(str) {
+    const jsonRegex = /\{.*\}/gs;
+    const match = str.match(jsonRegex);
+    if (!match) {
+        return null; // no JSON object found
+    }
+    return match[0];
+}
+
+app.post("/chat", async (req, res) => {
+    try {
+        const userResponse = req.body.userResponse;
+        const story = req.body.story;
+        if (!userResponse)
+            return res.status(400).send("userResponse is required");
+        if (!story) return res.status(400).send("story is required");
+        const completion = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [
+                {
+                    role: "system",
+                    content:
+                        'return the data in the following format only {"image":"description for a story related image", "story":"continue the story...", "enemy":trueOrFalse, "playerHealth":"x/100", "enemyHealth":"x/100", "options":["option1","option2","option3","option4"] }',
+                },
+                { role: "assistant", content: story },
+                { role: "user", content: userResponse },
+            ],
+        });
+        res.send(
+            JSON.parse(
+                extractJsonFromString(
+                    completion.data.choices[0].message.content
+                )
+            )
+        );
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error);
+    }
+});
+
+app.post("/image", async (req, res) => {
+    try {
+        const prompt = req.body.prompt + ", in starry night style";
+        if (!prompt) return res.status(400).send("prompt is required");
+        const response = await openai.createImage({
+            prompt: prompt,
+            n: 1,
+            size: "256x256",
+        });
+        const responseData = {
+            image_url: response.data.data[0].url,
+        };
+        res.send(responseData);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error);
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+});
